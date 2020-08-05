de6be53ffe082572679bd10dca06073ed65421ad
dopebear23 <palashjain2312000@gmail.com>
2020-08-05 21:11:22 +0530

    REACT commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -150,6 +151,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
   try {
     // @todo - remove user's posts
+    await Post.deleteMany({ user: req.user.id });
 
     //Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
